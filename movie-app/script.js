c4d1bc1a9049bcfa4bfa1ae21d4b3f2191d8803e
dejosemario <josemariaodumodu@gmail.com>
2020-11-27 21:48:29 +0100

    code to get the movie app data online using async method

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,1 +1,20 @@
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1'
+console.log(APIURL);
+const ImgPath = 'https://image.tmdb.org/t/p/w1280';
 
+async function getMovies(){
+   const resp = await fetch(APIURL);
+   const respData = await resp.json();
+   console.log(respData);
+
+   respData.results.forEach((movie) =>{
+     const img = document.createElement('img');
+     img.src = ImgPath + movie.poster_path;
+     document.body.appendChild(img);
+   })
+
+   return respData;
+
+}
+
+getMovies();
