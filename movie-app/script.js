9e19cae31f4bd145c8f167dfb7611f2440da85da
dejosemario <josemariaodumodu@gmail.com>
2020-12-02 13:13:43 +0100

    adding differnt colors to the ratings

--- movie-app/script.js
+++ movie-app/script.js
@@ -2,15 +2,26 @@ const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 console.log(APIURL);
 const ImgPath = 'https://image.tmdb.org/t/p/w1280';
 
+const main = document.querySelector('main');
+
 async function getMovies(){
    const resp = await fetch(APIURL);
    const respData = await resp.json();
    console.log(respData);
+  
 
    respData.results.forEach((movie) =>{
-     const img = document.createElement('img');
-     img.src = ImgPath + movie.poster_path;
-     document.body.appendChild(img);
+    const {poster_path, title, vote_average}= movie
+    const movieE1 = document.createElement('div'); 
+    movieE1.classList.add('movie');
+    movieE1.innerHTML = `
+        <img src="${ImgPath + poster_path}" alt="">
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getColors(vote_average)}">${vote_average}</span>
+        </div>      
+    `
+    main.appendChild(movieE1);
    })
 
    return respData;
@@ -18,3 +29,14 @@ async function getMovies(){
 }
 
 getMovies();
+
+//changing the color of the rating
+function getColors(vote){
+  if(vote >= 7){
+    return 'green';
+  }else if(vote >= 5){
+    return 'orange';
+  }else{
+     return 'red';
+  }  
+}
