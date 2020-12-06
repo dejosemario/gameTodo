8a631200fc3afe0ba7705e094e856c64a8924fb9
dejosemario <josemariaodumodu@gmail.com>
2020-12-06 22:38:06 +0100

    created the search function button and got to make it work with the search api

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,16 +1,28 @@
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1'
 console.log(APIURL);
 const ImgPath = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main = document.querySelector('main');
+const form = document.getElementById('form'); 
+const search = document.getElementById('search'); 
 
-async function getMovies(){
-   const resp = await fetch(APIURL);
+
+async function getMovies(url){
+   const resp = await fetch(url);
    const respData = await resp.json();
-   console.log(respData);
-  
+  //  console.log(respData);  
 
-   respData.results.forEach((movie) =>{
+  showMovies(respData.results);
+}
+
+getMovies(APIURL);
+
+function showMovies(movies){
+  //clear the movies
+   main.innerHTML = '';
+  movies.forEach((movie) =>{
     const {poster_path, title, vote_average}= movie
     const movieE1 = document.createElement('div'); 
     movieE1.classList.add('movie');
@@ -23,12 +35,7 @@ async function getMovies(){
     `
     main.appendChild(movieE1);
    })
-
-   return respData;
-
 }
-
-getMovies();
 
 //changing the color of the rating
 function getColors(vote){
@@ -40,3 +47,13 @@ function getColors(vote){
      return 'red';
   }  
 }
+//trying to get the value in the form attribute when submitted
+form.addEventListener('submit', (e)=>{
+e.preventDefault();
+
+searchResult = search.value;
+ if(searchResult){
+   getMovies(SEARCHAPI + searchResult);
+   search.value = '';
+ }
+})
