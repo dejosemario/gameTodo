5aed154260c98328d6b614af561f3d33c463535a
dejosemario <josemariaodumodu@gmail.com>
2021-01-04 10:51:34 +0100

    added the add/subtract buttons

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -1,7 +1,12 @@
 const c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
 
+const add = document.getElementById('addittion');
+const size = document.getElementById('numTally');
+const reduce = document.getElementById('reduction');
+
 let isNotClicked = true;
+let sizeE1 = 30;
 
 //creating a way to show when the mouse hasn't been clicked
 c.addEventListener('mouseup', ()=>{
@@ -16,8 +21,8 @@ c.addEventListener('mousedown', ()=>{
 //added an event listener to the canvas in other to get the property of the x and y axis when clicked.
 c.addEventListener('mousemove', (e)=>{
   if(!isNotClicked){
-    let x = e.offsetX;
-    let y = e.offsetY;
+    const x = e.offsetX;
+    const y = e.offsetY;
     drawCircle(x,y);
   }  
 })
@@ -28,3 +33,35 @@ function drawCircle(x,y){
   ctx.arc(x, y, 40, 0, 2 * Math.PI);
   ctx.fill();
 }
+
+
+
+//added a listner to the increase the mouse cursor when clicked
+add.addEventListener('click', ()=>{
+  sizeChange()
+  if(sizeE1 < 50){
+    sizeE1  += 5
+  }
+})
+
+//addedd an event listner to the reduce button to reduce the size of the mouse cursor for drawing when clicked.
+reduce.addEventListener('click', ()=>{
+  sizeChange()
+  if(sizeE1 > 5){
+    sizeE1  -= 5
+  }
+})
+
+//creating a fun. to change the innerText of the size div.
+function sizeChange(){
+  size.innerText = sizeE1
+}
+
+sizeChange();
+
+function drawCircle(x,y){
+  ctx.beginPath();
+  ctx.arc(x, y, sizeE1, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
